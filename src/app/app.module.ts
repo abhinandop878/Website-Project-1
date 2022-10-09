f6062571fa85b41ef6ce570fe01e3fdac9f7d9ec
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-09 13:29:03 +0530

    routes program added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,24 @@ import { GalleryComponent } from './gallery/gallery.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { RouterModule, Routes } from '@angular/router';
+const appRoutes:Routes=[
+{
+  path:"",component:IndexComponent
+},
+{
+  path:"mybook",component:MybookComponent
+},
+{
+  path:"gallery",component:GalleryComponent
+},
+{
+  path:"aboutus",component:AboutusComponent
+},
+{
+  path:"login",component:LoginComponent
+}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +41,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
